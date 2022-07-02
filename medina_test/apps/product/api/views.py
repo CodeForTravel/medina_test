d313be383bc6a966203fa06db4aaee79fd52271b
Mohammad Faisal <mdfaisalnstu@gmail.com>
2022-07-02 14:44:33 +0600

    product_recommendation created

--- medina_test/apps/product/api/views.py
+++ medina_test/apps/product/api/views.py
@@ -1,4 +1,6 @@
-from rest_framework import viewsets, filters
+from rest_framework import viewsets, filters, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated, AllowAny
 
 from medina_test.apps.product.api import filters as filters_product
@@ -37,8 +39,16 @@ class ProductViewSet(viewsets.ModelViewSet):
         """
         Instantiates and returns the list of permissions that this view requires.
         """
-        if self.action in ["retrieve", "list"]:
+        if self.action in ["retrieve", "list", "product_recommendation"]:
             permission_classes = [AllowAny]
         else:
             permission_classes = [IsAuthenticated & permissions_user.VendorUserOnly]
         return [permission() for permission in permission_classes]
+
+    @action(detail=False, methods=['get'], url_path="recommendations")
+    def product_recommendation(self, request):
+        products = self.get_queryset()
+        serializer = serializers_product.ProductSerializer(products,many=True)
+        return Response({'data': serializer.data},status=status.HTTP_200_OK)
+ 
+        
